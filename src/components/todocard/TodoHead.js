a690145de705ecaa9db631fa1ffae725e00eeb44
powedonk123 <powedonk123@naver.com>
2020-11-02 09:20:12 +0000

    날짜 요일 추가

--- src/components/todocard/TodoHead.js
+++ src/components/todocard/TodoHead.js
@@ -9,20 +9,20 @@ const TodoHead = () => {
 	
 	const onViewOff = useContext(AllTodoViewOffContext);
 	
-	/*const today = new Date();
+	const today = new Date(date);
 	const dateString = today.toLocaleDateString('ko-KR', {
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric',
 	});
-	const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });*/
+	const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
 
 	if(!todos) return <div>Loding...</div>;
 	return (
 		<div className="todo-head-block">
 			<a className="toto-view-close" onClick={onViewOff}>X</a>
-			<h1>{date}</h1>
-			<div className="day">요일 개발중..., keyid={keyid}</div>
+			<h1>{dateString}</h1>
+			<div className="day">{dayName}, keyid={keyid}</div>
 			<div className="tasks-left">할 일 - {undoneTodos.length}개</div>
 		</div>
 	);
